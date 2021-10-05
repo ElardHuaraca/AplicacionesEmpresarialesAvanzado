cbbbeb9e8d22e33f349f32da4a87a81f5504fa7a
Elard <elard.sd.9@gmail.com>
2021-10-04 22:33:11 -0500

    Lab echo

---
+++ Semana07/ViewModel/ListaCategoriaViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using Semana07.View;
+
+namespace Semana07.ViewModel
+{
+    public class ListaCategoriaViewModel : ViewModelBase
+    {
+
+        ObservableCollection<Categoria> _Categorias;
+        public ObservableCollection<Categoria> Categorias{ get; set;}
+        
+
+        public ICommand NuevoCommand { set; get; }
+        public ICommand ConsultarCommand { set; get; }
+
+        public ListaCategoriaViewModel()
+        {
+            Categorias = new Model.CategoriaModel().Categorias;
+
+            NuevoCommand = new RelayCommand<Window>(
+                param => Abrir()
+                );
+
+            ConsultarCommand = new RelayCommand<object>(
+                o => { Categorias = (new Model.CategoriaModel()).Categorias; }
+                );
+
+            void Abrir()
+            {
+                ManCategoria window = new ManCategoria();
+                window.ShowDialog();
+                
+            }
+        }
+    }
+}
