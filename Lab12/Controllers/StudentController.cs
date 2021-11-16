244baf90364653557fcb3756727b71a205fdb81c
Elard Huaraca <56555556+jajanotecreo@users.noreply.github.com>
2021-11-15 23:47:05 -0500

    Lab terminado

---
+++ Lab12/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using Domain;
+using Lab12.Models;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab12.Controllers
+{
+    public class StudentController : Controller
+    {
+        private StudentService studentService = new StudentService();
+        // GET: Student
+        public ActionResult IndexRazor()
+        {
+            var model = (from c in studentService.Get()
+                         select new StudentModel
+                         {
+                             ID = c.studentId,
+                             Address = c.studentAddress,
+                             Name = c.studentName
+                         });
+            return View(model);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getStudent(string id)
+        {
+            return Json(studentService.Get(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult createStudent(Student std)
+        {
+            studentService.Insert(std);
+            string msg = "SUCCESS";
+            return Json(new { Message = msg, JsonRequestBehavior.AllowGet });
+        }
+    }
+}
---
+++ Lab12/Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab12.Models
+{
+    public partial class StudentModel
+    {
+        public int ID { get; set; }
+        public string Address { get; set; }
+        public string Name { get; set; }
+    }
+}
