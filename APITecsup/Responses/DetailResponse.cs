c8310bbca48f0675842fb2a1c41a3b922601b6a0
Elard Huaraca <56555556+jajanotecreo@users.noreply.github.com>
2021-11-28 15:22:49 -0500

    Lab Terminado V2

---
+++ APITecsup/Responses/DetailResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APITecsup.Responses
+{
+    public class DetailResponse
+    {
+        public int Count { get; set; }
+        public int Price { get; set; }
+        public int SubTotal { get; set; }
+        public double IGV { get; set; }
+        public double Total { get; set; }
+    }
+}
