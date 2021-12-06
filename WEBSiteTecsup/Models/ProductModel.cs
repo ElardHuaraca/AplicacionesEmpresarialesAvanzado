3d752ff110fddac076f0d5304193af7b0dce20c0
Elard Huaraca <56555556+jajanotecreo@users.noreply.github.com>
2021-12-05 21:38:42 -0500

    Lab15 Terminado

---
+++ WEBSiteTecsup/Models/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEBSiteTecsup.Models
+{
+    public class ProductModel
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
