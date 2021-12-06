a494248c51ee42b41f4248936423b14fe9c1e4a6
Elard Huaraca <56555556+jajanotecreo@users.noreply.github.com>
2021-12-05 21:37:08 -0500

    Lab15 Terminado

---
+++ APITecsup/Migrations/202112060147049_Update database.cs
@@ -0,0 +1,40 @@
+﻿namespace APITecsup.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Updatedatabase : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Invoices", "CustomerID", "dbo.Customers");
+            DropIndex("dbo.Invoices", new[] { "CustomerID" });
+            AlterColumn("dbo.Invoices", "Total", c => c.Int(nullable: false));
+            DropColumn("dbo.Details", "IGV");
+            DropColumn("dbo.Details", "Total");
+            DropColumn("dbo.Invoices", "CustomerID");
+            DropTable("dbo.Customers");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Address = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Invoices", "CustomerID", c => c.Int(nullable: false));
+            AddColumn("dbo.Details", "Total", c => c.Double(nullable: false));
+            AddColumn("dbo.Details", "IGV", c => c.Double(nullable: false));
+            AlterColumn("dbo.Invoices", "Total", c => c.Double(nullable: false));
+            CreateIndex("dbo.Invoices", "CustomerID");
+            AddForeignKey("dbo.Invoices", "CustomerID", "dbo.Customers", "Id", cascadeDelete: true);
+        }
+    }
+}
